71d78acc0a11ce8bf837ef56aedb67aed0571b26
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-08-21 12:34:22 +0300

    add jsons

---
+++ db2jsons.sh
@@ -0,0 +1,24 @@
+
+data=$(cat db.csv | cut -d';' -f1,2,6)
+
+dir=inputs
+rm -rf $dir
+mkdir $dir
+
+echo "$data" | while read line 
+do
+
+    IFS=';'
+    read -a arr <<<"$line" 
+
+    number=${arr[0]}
+    name=$(echo "${arr[1]}" | tr ' ' '_')
+    json=${arr[2]}
+
+    echo processing $number $name
+
+    js=$dir/$name.json
+    echo "$json" | python -m json.tool --no-ensure-ascii > $js
+
+
+done
