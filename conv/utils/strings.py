3f00d762eb42450f24522c489adc6143b5ec1899
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-08-22 11:29:20 +0300

    -

--- conv/utils/strings.py
+++ conv/utils/strings.py
@@ -1,9 +1,22 @@
 
 
 def fix_string_name(s: str) -> str:
+    """
+    fixes string to correct python name
+
+    >>> _ = fix_string_name
+    >>> _('a')
+    'a'
+    >>> _('a b.c-e')
+    'a_b_c_e'
+    """
     return s.replace(' ', '_').replace('-', '_').replace('.', '_')
 
 
 def name_to_tree_call(s: str) -> str:
+    """
+    >>> name_to_tree_call('name')
+    '___{{ name }}'
+    """
     # return f"___{sum(ord(_s) for _s in s)}"
     return "___{{ " + s + " }}"
