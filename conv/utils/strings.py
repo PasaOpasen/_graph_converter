61f2e5c850e81db8d1aee81b2613f97956cc917f
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-08-23 14:11:57 +0300

    -

--- conv/utils/strings.py
+++ conv/utils/strings.py
@@ -1,5 +1,5 @@
 
-from typing import Dict, Tuple, Sequence, List
+from typing import Dict, Tuple, Sequence, List, Callable, Any
 
 import re
 
@@ -26,6 +26,8 @@ def name_to_tree_call(s: str) -> str:
     return "___{{ " + s + " }}"
 
 
+#region JINJA VARIABLES
+
 _var_pattern = re.compile(r"\{\{\s[^\{}]*\s}}")
 
 
@@ -45,6 +47,48 @@ def get_string_vars(s: str) -> Dict[Tuple[int, int], str]:
         (m.start(), m.end()): s[m.start(): m.end()].strip('{ }')
         for m in _var_pattern.finditer(s)
     }
+
+
+def replace_string_vars(s: str, name_resolver: Callable[[str], Any]) -> str:
+    """
+    replaces jinja vars in the string
+    Args:
+        s:
+        name_resolver: function returns variables values for variables names
+
+    Returns:
+        new string
+
+    >>> _ = replace_string_vars
+    >>> variables = dict(a=1, b=2, c=3)
+    >>> resolver = variables.get
+    >>> assert _(s='', name_resolver=resolver) == ''
+    >>> assert _('no vars', resolver) == 'no vars'
+    >>> _('{{ a }} + {{ b }} == {{ c }}', resolver)
+    '1 + 2 == 3'
+    """
+    if not s:
+        return s
+
+    vars_data = get_string_vars(s)
+    if not vars_data:
+        return s
+
+    all_vars = set(vars_data.values())
+    var2val = {
+        v: str(name_resolver(v))
+        for v in all_vars
+    }
+
+    s = list(s)
+    for (start, end), name in sorted(vars_data.items(), reverse=True):
+        s[start:end] = list(
+            var2val[name]
+        )
+
+    return ''.join(s)
+
+#endregion
 
 
 def add_numbers_to_sorted(seq: Sequence, sep: str = '.') -> List[str]:
